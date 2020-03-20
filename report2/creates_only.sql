2b36c50ca6cc9d5be6ade911d84ded814671374f
Larry Lai <dppss91102@gmail.com>
2020-03-20 14:21:40 -0400

    Modify CREATE TABLE

--- report2/creates_only.sql
+++ report2/creates_only.sql
@@ -11,9 +11,9 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for table zli58.articles_chapters
-DROP TABLE IF EXISTS `articles_chapters`;
-CREATE TABLE IF NOT EXISTS `articles_chapters` (
+-- Dumping structure for table zli58.Articles_Chapters
+DROP TABLE IF EXISTS `Articles_Chapters`;
+CREATE TABLE IF NOT EXISTS `Articles_Chapters` (
   `art_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `text` text NOT NULL,
@@ -21,68 +21,68 @@ CREATE TABLE IF NOT EXISTS `articles_chapters` (
   `topic` varchar(128) DEFAULT NULL,
   `pub_id` int(11) NOT NULL,
   PRIMARY KEY (`art_id`),
-  KEY `FK_articles_chapters_publications` (`pub_id`),
-  CONSTRAINT `FK_articles_chapters_publications` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `FK_Articles_Chapters_Publications` (`pub_id`),
+  CONSTRAINT `FK_Articles_Chapters_Publications` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.authors
-DROP TABLE IF EXISTS `authors`;
-CREATE TABLE IF NOT EXISTS `authors` (
+-- Dumping structure for table zli58.Authors
+DROP TABLE IF EXISTS `Authors`;
+CREATE TABLE IF NOT EXISTS `Authors` (
   `person_id` int(11) NOT NULL,
   `type` char(16) NOT NULL,
   PRIMARY KEY (`person_id`),
-  CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Authors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.author_write_articles_or_chapters
-DROP TABLE IF EXISTS `author_write_articles_or_chapters`;
-CREATE TABLE IF NOT EXISTS `author_write_articles_or_chapters` (
+-- Dumping structure for table zli58.Author_write_articles_or_chapters
+DROP TABLE IF EXISTS `Author_write_articles_or_chapters`;
+CREATE TABLE IF NOT EXISTS `Author_write_articles_or_chapters` (
   `person_id` int(11) NOT NULL,
   `art_id` int(11) NOT NULL,
   `pub_id` int(11) NOT NULL,
   PRIMARY KEY (`person_id`,`art_id`),
   KEY `art_id` (`art_id`),
-  KEY `FK_author_write_articles_or_chapters_publications` (`pub_id`),
-  CONSTRAINT `FK_author_write_articles_or_chapters_publications` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `author_write_articles_or_chapters_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `author_write_articles_or_chapters_ibfk_2` FOREIGN KEY (`art_id`) REFERENCES `articles_chapters` (`art_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `FK_Author_write_articles_or_chapters_Publications` (`pub_id`),
+  CONSTRAINT `FK_Author_write_articles_or_chapters_Publications` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Author_write_articles_or_chapters_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Author_write_articles_or_chapters_ibfk_2` FOREIGN KEY (`art_id`) REFERENCES `Articles_Chapters` (`art_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.author_write_books
-DROP TABLE IF EXISTS `author_write_books`;
-CREATE TABLE IF NOT EXISTS `author_write_books` (
+-- Dumping structure for table zli58.Author_write_Books
+DROP TABLE IF EXISTS `Author_write_Books`;
+CREATE TABLE IF NOT EXISTS `Author_write_Books` (
   `person_id` int(11) NOT NULL,
   `pub_id` int(11) NOT NULL,
   PRIMARY KEY (`person_id`,`pub_id`),
   KEY `pub_id` (`pub_id`),
-  CONSTRAINT `author_write_books_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `author_write_books_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `books` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Author_write_Books_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Author_write_Books_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Books` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.books
-DROP TABLE IF EXISTS `books`;
-CREATE TABLE IF NOT EXISTS `books` (
+-- Dumping structure for table zli58.Books
+DROP TABLE IF EXISTS `Books`;
+CREATE TABLE IF NOT EXISTS `Books` (
   `pub_id` int(11) NOT NULL,
   `edition` int(11) DEFAULT NULL,
   `ISBN` varchar(120) DEFAULT NULL,
   `topic` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`pub_id`),
-  CONSTRAINT `books_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Books_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.distributors
-DROP TABLE IF EXISTS `distributors`;
-CREATE TABLE IF NOT EXISTS `distributors` (
+-- Dumping structure for table zli58.Distributors
+DROP TABLE IF EXISTS `Distributors`;
+CREATE TABLE IF NOT EXISTS `Distributors` (
   `person_id` int(11) NOT NULL,
   `balance` decimal(10,2) NOT NULL,
   `contact_person` varchar(128) NOT NULL,
@@ -92,49 +92,49 @@ CREATE TABLE IF NOT EXISTS `distributors` (
   `name` varchar(128) NOT NULL,
   `address` varchar(128) NOT NULL,
   PRIMARY KEY (`person_id`),
-  CONSTRAINT `distributors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Distributors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.editors
-DROP TABLE IF EXISTS `editors`;
-CREATE TABLE IF NOT EXISTS `editors` (
+-- Dumping structure for table zli58.Editors
+DROP TABLE IF EXISTS `Editors`;
+CREATE TABLE IF NOT EXISTS `Editors` (
   `person_id` int(11) NOT NULL,
   `type` char(16) NOT NULL,
   PRIMARY KEY (`person_id`),
-  CONSTRAINT `editors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Editors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.editor_edit_publications
-DROP TABLE IF EXISTS `editor_edit_publications`;
-CREATE TABLE IF NOT EXISTS `editor_edit_publications` (
+-- Dumping structure for table zli58.Editor_edit_Publications
+DROP TABLE IF EXISTS `Editor_edit_Publications`;
+CREATE TABLE IF NOT EXISTS `Editor_edit_Publications` (
   `person_id` int(11) NOT NULL,
   `pub_id` int(11) NOT NULL,
   PRIMARY KEY (`person_id`,`pub_id`),
   KEY `pub_id` (`pub_id`),
-  CONSTRAINT `editor_edit_publications_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `editors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `editor_edit_publications_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Editor_edit_Publications_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Editors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Editor_edit_Publications_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.issues
-DROP TABLE IF EXISTS `issues`;
-CREATE TABLE IF NOT EXISTS `issues` (
+-- Dumping structure for table zli58.Issues
+DROP TABLE IF EXISTS `Issues`;
+CREATE TABLE IF NOT EXISTS `Issues` (
   `pub_id` int(11) NOT NULL,
   `period_id` int(11) NOT NULL,
   PRIMARY KEY (`pub_id`),
-  CONSTRAINT `issues_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Issues_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.orders
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
+-- Dumping structure for table zli58.Orders
+DROP TABLE IF EXISTS `Orders`;
+CREATE TABLE IF NOT EXISTS `Orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `num_of_copy` int(11) NOT NULL,
   `date` date NOT NULL,
@@ -145,15 +145,15 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`order_id`),
   KEY `person_id` (`person_id`),
   KEY `pub_id` (`pub_id`),
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `distributors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Distributors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.payments
-DROP TABLE IF EXISTS `payments`;
-CREATE TABLE IF NOT EXISTS `payments` (
+-- Dumping structure for table zli58.Payments
+DROP TABLE IF EXISTS `Payments`;
+CREATE TABLE IF NOT EXISTS `Payments` (
   `pay_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `type` varchar(128) NOT NULL,
@@ -164,9 +164,9 @@ CREATE TABLE IF NOT EXISTS `payments` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.periodicals
-DROP TABLE IF EXISTS `periodicals`;
-CREATE TABLE IF NOT EXISTS `periodicals` (
+-- Dumping structure for table zli58.Periodicals
+DROP TABLE IF EXISTS `Periodicals`;
+CREATE TABLE IF NOT EXISTS `Periodicals` (
   `period_id` int(11) NOT NULL AUTO_INCREMENT,
   `periodicity` varchar(20) NOT NULL,
   `topic` varchar(128) DEFAULT NULL,
@@ -175,9 +175,9 @@ CREATE TABLE IF NOT EXISTS `periodicals` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.persons
-DROP TABLE IF EXISTS `persons`;
-CREATE TABLE IF NOT EXISTS `persons` (
+-- Dumping structure for table zli58.Persons
+DROP TABLE IF EXISTS `Persons`;
+CREATE TABLE IF NOT EXISTS `Persons` (
   `person_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(128) NOT NULL,
   `name` varchar(128) NOT NULL,
@@ -186,9 +186,9 @@ CREATE TABLE IF NOT EXISTS `persons` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table zli58.publications
-DROP TABLE IF EXISTS `publications`;
-CREATE TABLE IF NOT EXISTS `publications` (
+-- Dumping structure for table zli58.Publications
+DROP TABLE IF EXISTS `Publications`;
+CREATE TABLE IF NOT EXISTS `Publications` (
   `pub_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
   `date` date NOT NULL,
