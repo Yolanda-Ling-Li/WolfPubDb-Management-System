5574826af4de7a54f25d991fa6ae41ee2443e49d
Larry Lai <dppss91102@gmail.com>
2020-03-20 14:25:19 -0400

    Modify INSERT INTO

--- report2/inserts_only.sql
+++ report2/inserts_only.sql
@@ -11,9 +11,9 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping data for table zli58.articles_chapters: ~8 rows (approximately)
-/*!40000 ALTER TABLE `articles_chapters` DISABLE KEYS */;
-INSERT INTO `articles_chapters` (`art_id`, `date`, `text`, `title`, `topic`) VALUES
+-- Dumping data for table zli58.Articles_Chapters: ~8 rows (approximately)
+/*!40000 ALTER TABLE `Articles_Chapters` DISABLE KEYS */;
+INSERT INTO `Articles_Chapters` (`art_id`, `date`, `text`, `title`, `topic`) VALUES
 	(1, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
 	(2, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
 	(3, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
@@ -22,110 +22,110 @@ INSERT INTO `articles_chapters` (`art_id`, `date`, `text`, `title`, `topic`) VAL
 	(6, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
 	(7, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
 	(8, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL);
-/*!40000 ALTER TABLE `articles_chapters` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Articles_Chapters` ENABLE KEYS */;
 
--- Dumping data for table zli58.authors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` (`person_id`, `type`) VALUES
+-- Dumping data for table zli58.Authors: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Authors` DISABLE KEYS */;
+INSERT INTO `Authors` (`person_id`, `type`) VALUES
 	(2, 'staff'),
 	(8, 'invited'),
 	(9, 'staff'),
 	(10, 'invited');
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Authors` ENABLE KEYS */;
 
--- Dumping data for table zli58.author_write_articles_or_chapters: ~4 rows (approximately)
-/*!40000 ALTER TABLE `author_write_articles_or_chapters` DISABLE KEYS */;
-INSERT INTO `author_write_articles_or_chapters` (`person_id`, `art_id`, `pub_id`) VALUES
+-- Dumping data for table zli58.Author_write_articles_or_chapters: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Author_write_articles_or_chapters` DISABLE KEYS */;
+INSERT INTO `Author_write_articles_or_chapters` (`person_id`, `art_id`, `pub_id`) VALUES
 	(2, 1, 1),
 	(2, 2, 1),
 	(2, 3, 2),
 	(2, 4, 2);
-/*!40000 ALTER TABLE `author_write_articles_or_chapters` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Author_write_articles_or_chapters` ENABLE KEYS */;
 
--- Dumping data for table zli58.author_write_books: ~4 rows (approximately)
-/*!40000 ALTER TABLE `author_write_books` DISABLE KEYS */;
-INSERT INTO `author_write_books` (`person_id`, `pub_id`) VALUES
+-- Dumping data for table zli58.Author_write_Books: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Author_write_Books` DISABLE KEYS */;
+INSERT INTO `Author_write_Books` (`person_id`, `pub_id`) VALUES
 	(10, 1),
 	(10, 2),
 	(10, 3),
 	(10, 4);
-/*!40000 ALTER TABLE `author_write_books` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Author_write_Books` ENABLE KEYS */;
 
--- Dumping data for table zli58.books: ~4 rows (approximately)
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` (`pub_id`, `edition`, `ISBN`, `topic`) VALUES
+-- Dumping data for table zli58.Books: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Books` DISABLE KEYS */;
+INSERT INTO `Books` (`pub_id`, `edition`, `ISBN`, `topic`) VALUES
 	(1, 1, '123', NULL),
 	(2, 1, '123', NULL),
 	(3, 1, '123', NULL),
 	(4, 1, '123', NULL);
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Books` ENABLE KEYS */;
 
--- Dumping data for table zli58.distributors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `distributors` DISABLE KEYS */;
-INSERT INTO `distributors` (`person_id`, `balance`, `contact_person`, `phone_num`, `type`, `city`, `name`, `address`) VALUES
+-- Dumping data for table zli58.Distributors: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Distributors` DISABLE KEYS */;
+INSERT INTO `Distributors` (`person_id`, `balance`, `contact_person`, `phone_num`, `type`, `city`, `name`, `address`) VALUES
 	(4, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
 	(5, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
 	(6, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
 	(7, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown');
-/*!40000 ALTER TABLE `distributors` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Distributors` ENABLE KEYS */;
 
--- Dumping data for table zli58.editors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `editors` DISABLE KEYS */;
-INSERT INTO `editors` (`person_id`, `type`) VALUES
+-- Dumping data for table zli58.Editors: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Editors` DISABLE KEYS */;
+INSERT INTO `Editors` (`person_id`, `type`) VALUES
 	(3, 'staff'),
 	(11, 'invited'),
 	(12, 'staff'),
 	(16, 'staff');
-/*!40000 ALTER TABLE `editors` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Editors` ENABLE KEYS */;
 
--- Dumping data for table zli58.editor_edit_publications: ~4 rows (approximately)
-/*!40000 ALTER TABLE `editor_edit_publications` DISABLE KEYS */;
-INSERT INTO `editor_edit_publications` (`person_id`, `pub_id`) VALUES
+-- Dumping data for table zli58.Editor_edit_Publications: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Editor_edit_Publications` DISABLE KEYS */;
+INSERT INTO `Editor_edit_Publications` (`person_id`, `pub_id`) VALUES
 	(12, 5),
 	(16, 2),
 	(16, 5),
 	(16, 7);
-/*!40000 ALTER TABLE `editor_edit_publications` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Editor_edit_Publications` ENABLE KEYS */;
 
--- Dumping data for table zli58.issues: ~4 rows (approximately)
-/*!40000 ALTER TABLE `issues` DISABLE KEYS */;
-INSERT INTO `issues` (`pub_id`, `period_id`) VALUES
+-- Dumping data for table zli58.Issues: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Issues` DISABLE KEYS */;
+INSERT INTO `Issues` (`pub_id`, `period_id`) VALUES
 	(5, 1),
 	(6, 1),
 	(7, 1),
 	(8, 2);
-/*!40000 ALTER TABLE `issues` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Issues` ENABLE KEYS */;
 
--- Dumping data for table zli58.orders: ~4 rows (approximately)
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` (`order_id`, `num_of_copy`, `date`, `price`, `shipping_cost`, `person_id`, `pub_id`) VALUES
+-- Dumping data for table zli58.Orders: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+INSERT INTO `Orders` (`order_id`, `num_of_copy`, `date`, `price`, `shipping_cost`, `person_id`, `pub_id`) VALUES
 	(1, 1, '2020-03-17', 100.00, 100.00, 6, 1),
 	(2, 1, '2020-03-17', 100.00, 100.00, 6, 1),
 	(3, 1, '2020-03-17', 100.00, 100.00, 6, 1),
 	(4, 1, '2020-03-17', 100.00, 100.00, 6, 1);
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
 
--- Dumping data for table zli58.payments: ~4 rows (approximately)
-/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
-INSERT INTO `payments` (`pay_id`, `date`, `type`, `amount`, `person_id`) VALUES
+-- Dumping data for table zli58.Payments: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Payments` DISABLE KEYS */;
+INSERT INTO `Payments` (`pay_id`, `date`, `type`, `amount`, `person_id`) VALUES
 	(1, '2020-03-17', 'one-time', 100.00, 1),
 	(2, '2020-03-17', 'monthly', 1.00, 2),
 	(3, '2020-03-17', 'weekly', 200.00, 3),
 	(4, '2020-03-15', 'one-time', 3.00, 4);
-/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Payments` ENABLE KEYS */;
 
--- Dumping data for table zli58.periodicals: ~4 rows (approximately)
-/*!40000 ALTER TABLE `periodicals` DISABLE KEYS */;
-INSERT INTO `periodicals` (`period_id`, `periodicity`, `topic`) VALUES
+-- Dumping data for table zli58.Periodicals: ~4 rows (approximately)
+/*!40000 ALTER TABLE `Periodicals` DISABLE KEYS */;
+INSERT INTO `Periodicals` (`period_id`, `periodicity`, `topic`) VALUES
 	(1, 'monthly', NULL),
 	(2, 'weekly', NULL),
 	(3, 'weekly', NULL),
 	(4, 'weekly', NULL);
-/*!40000 ALTER TABLE `periodicals` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Periodicals` ENABLE KEYS */;
 
--- Dumping data for table zli58.persons: ~16 rows (approximately)
-/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
-INSERT INTO `persons` (`person_id`, `type`, `name`) VALUES
+-- Dumping data for table zli58.Persons: ~16 rows (approximately)
+/*!40000 ALTER TABLE `Persons` DISABLE KEYS */;
+INSERT INTO `Persons` (`person_id`, `type`, `name`) VALUES
 	(1, 'admin', 'Alice'),
 	(2, 'author', 'Bob'),
 	(3, 'editor', 'Emma'),
@@ -142,11 +142,11 @@ INSERT INTO `persons` (`person_id`, `type`, `name`) VALUES
 	(14, 'admin', 'K'),
 	(15, 'admin', 'L'),
 	(16, 'editor', 'M');
-/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Persons` ENABLE KEYS */;
 
--- Dumping data for table zli58.publications: ~8 rows (approximately)
-/*!40000 ALTER TABLE `publications` DISABLE KEYS */;
-INSERT INTO `publications` (`pub_id`, `title`, `date`) VALUES
+-- Dumping data for table zli58.Publications: ~8 rows (approximately)
+/*!40000 ALTER TABLE `Publications` DISABLE KEYS */;
+INSERT INTO `Publications` (`pub_id`, `title`, `date`) VALUES
 	(1, 'Book1', '2020-03-17'),
 	(2, 'Book2', '2020-03-17'),
 	(3, 'Book3', '2020-03-17'),
@@ -155,7 +155,7 @@ INSERT INTO `publications` (`pub_id`, `title`, `date`) VALUES
 	(6, 'Issue2', '2020-03-17'),
 	(7, 'Issue3', '2020-03-17'),
 	(8, 'Issue4', '2020-03-17');
-/*!40000 ALTER TABLE `publications` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Publications` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
