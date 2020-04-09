349072643dafd5b3c2995fd6c2aa2104d9cf68c1
Larry Lai <dppss91102@gmail.com>
2020-04-09 16:51:06 -0400

    Add demo data

--- sql/creates_only.sql
+++ sql/creates_only.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.5.1-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Dumping structure for table zli58.Articles_Chapters
 DROP TABLE IF EXISTS `Articles_Chapters`;
 CREATE TABLE IF NOT EXISTS `Articles_Chapters` (
   `art_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -23,38 +9,26 @@ CREATE TABLE IF NOT EXISTS `Articles_Chapters` (
   PRIMARY KEY (`art_id`),
   KEY `FK_Articles_Chapters_Publications` (`pub_id`),
   CONSTRAINT `FK_Articles_Chapters_Publications` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Authors
 DROP TABLE IF EXISTS `Authors`;
 CREATE TABLE IF NOT EXISTS `Authors` (
   `person_id` int(11) NOT NULL,
   `type` char(16) NOT NULL,
   PRIMARY KEY (`person_id`),
   CONSTRAINT `Authors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Author_write_Articles_or_Chapters
 DROP TABLE IF EXISTS `Author_write_Articles_or_Chapters`;
 CREATE TABLE IF NOT EXISTS `Author_write_Articles_or_Chapters` (
   `person_id` int(11) NOT NULL,
   `art_id` int(11) NOT NULL,
-  `pub_id` int(11) NOT NULL,
   PRIMARY KEY (`person_id`,`art_id`),
   KEY `art_id` (`art_id`),
-  KEY `FK_Author_write_Articles_or_Chapters_Publications` (`pub_id`),
-  CONSTRAINT `FK_Author_write_Articles_or_Chapters_Publications` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Author_write_Articles_or_Chapters_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Author_write_Articles_or_Chapters_ibfk_2` FOREIGN KEY (`art_id`) REFERENCES `Articles_Chapters` (`art_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Author_write_Books
 DROP TABLE IF EXISTS `Author_write_Books`;
 CREATE TABLE IF NOT EXISTS `Author_write_Books` (
   `person_id` int(11) NOT NULL,
@@ -63,11 +37,8 @@ CREATE TABLE IF NOT EXISTS `Author_write_Books` (
   KEY `pub_id` (`pub_id`),
   CONSTRAINT `Author_write_Books_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Authors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Author_write_Books_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Books` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Books
 DROP TABLE IF EXISTS `Books`;
 CREATE TABLE IF NOT EXISTS `Books` (
   `pub_id` int(11) NOT NULL,
@@ -76,39 +47,27 @@ CREATE TABLE IF NOT EXISTS `Books` (
   `topic` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`pub_id`),
   CONSTRAINT `Books_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Distributors
 DROP TABLE IF EXISTS `Distributors`;
 CREATE TABLE IF NOT EXISTS `Distributors` (
   `person_id` int(11) NOT NULL,
   `balance` decimal(10,2) NOT NULL,
   `contact_person` varchar(128) NOT NULL,
-  `phone_num` varchar(20) NOT NULL,
   `type` varchar(128) NOT NULL,
   `city` varchar(128) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `address` varchar(128) NOT NULL,
   PRIMARY KEY (`person_id`),
   CONSTRAINT `Distributors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Editors
 DROP TABLE IF EXISTS `Editors`;
 CREATE TABLE IF NOT EXISTS `Editors` (
   `person_id` int(11) NOT NULL,
   `type` char(16) NOT NULL,
   PRIMARY KEY (`person_id`),
   CONSTRAINT `Editors_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Persons` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Editor_edit_Publications
 DROP TABLE IF EXISTS `Editor_edit_Publications`;
 CREATE TABLE IF NOT EXISTS `Editor_edit_Publications` (
   `person_id` int(11) NOT NULL,
@@ -117,27 +76,22 @@ CREATE TABLE IF NOT EXISTS `Editor_edit_Publications` (
   KEY `pub_id` (`pub_id`),
   CONSTRAINT `Editor_edit_Publications_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Editors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Editor_edit_Publications_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Issues
 DROP TABLE IF EXISTS `Issues`;
 CREATE TABLE IF NOT EXISTS `Issues` (
   `pub_id` int(11) NOT NULL,
   `period_id` int(11) NOT NULL,
   PRIMARY KEY (`pub_id`),
   CONSTRAINT `Issues_ibfk_1` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Orders
 DROP TABLE IF EXISTS `Orders`;
 CREATE TABLE IF NOT EXISTS `Orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `num_of_copy` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `order_date` date NOT NULL,
+  `delivery_date` date NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `shipping_cost` decimal(10,2) NOT NULL,
   `person_id` int(11) NOT NULL,
@@ -147,11 +101,8 @@ CREATE TABLE IF NOT EXISTS `Orders` (
   KEY `pub_id` (`pub_id`),
   CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `Distributors` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`pub_id`) REFERENCES `Publications` (`pub_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Payments
 DROP TABLE IF EXISTS `Payments`;
 CREATE TABLE IF NOT EXISTS `Payments` (
   `pay_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -160,43 +111,35 @@ CREATE TABLE IF NOT EXISTS `Payments` (
   `amount` decimal(10,2) NOT NULL,
   `person_id` int(11) NOT NULL,
   PRIMARY KEY (`pay_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Periodicals
 DROP TABLE IF EXISTS `Periodicals`;
 CREATE TABLE IF NOT EXISTS `Periodicals` (
   `period_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
   `periodicity` varchar(20) NOT NULL,
   `topic` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`period_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Persons
 DROP TABLE IF EXISTS `Persons`;
 CREATE TABLE IF NOT EXISTS `Persons` (
   `person_id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(128) NOT NULL,
   `name` varchar(128) NOT NULL,
+  `gender` varchar(8),
+  `age` int(11),
+  `email` varchar(8),
+  `phone_num` varchar(20),
+  `address` varchar(128),
   PRIMARY KEY (`person_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
--- Dumping structure for table zli58.Publications
 DROP TABLE IF EXISTS `Publications`;
 CREATE TABLE IF NOT EXISTS `Publications` (
   `pub_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`pub_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+);
 
--- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- sql/inserts_only.sql
+++ sql/inserts_only.sql
@@ -1,162 +1,61 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.5.1-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
+INSERT INTO Persons (person_id, type, name, gender, age, email, phone_num, address) VALUES
+	(1, 'admin', 'Alice', NULL, NULL, NULL, NULL, NULL),
+	(3001, 'editor', 'John', 'M', 36, '3001@gmail.com', '9391234567', '21 ABC St, NC 27'),
+	(3002, 'editor', 'Ethen', 'M', 30, '3002@gmail.com', '9491234567', '21 ABC St, NC 27606'),
+	(3003, 'author', 'Cathy', 'F', 28, '3003@gmail.com', '9591234567', '3300 AAA St, NC 27606'),
+	(2001, 'distributor', 'BookSell', NULL, NULL, NULL, '9191234567', '2200, A Street, NC'),
+	(2002, 'distributor', 'BookDist', NULL, NULL, NULL, '9291234567', '2200, B Street, NC');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+INSERT INTO Distributors (person_id, balance, contact_person, type, city) VALUES
+	(2001, 215.00, 'Jason', 'bookstore', 'charlotte'),
+	(2002, 0.00, 'Alex', 'wholesaler', 'Raleigh');
 
--- Dumping data for table zli58.Articles_Chapters: ~8 rows (approximately)
-/*!40000 ALTER TABLE `Articles_Chapters` DISABLE KEYS */;
-INSERT INTO `Articles_Chapters` (`art_id`, `date`, `text`, `title`, `topic`) VALUES
-	(1, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(2, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(3, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(4, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(5, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(6, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(7, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL),
-	(8, '2020-03-17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ', 'Yet another title', NULL);
-/*!40000 ALTER TABLE `Articles_Chapters` ENABLE KEYS */;
+INSERT INTO Publications (pub_id, title, date) VALUES
+	(1001, 'introduction to database', '2018-10-10'),
+	(1002, 'Healthy Diet', '2020-02-24'),
+	(1003, 'Animal Science', '2020-03-01');
 
--- Dumping data for table zli58.Authors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Authors` DISABLE KEYS */;
-INSERT INTO `Authors` (`person_id`, `type`) VALUES
-	(2, 'staff'),
-	(8, 'invited'),
-	(9, 'staff'),
-	(10, 'invited');
-/*!40000 ALTER TABLE `Authors` ENABLE KEYS */;
+INSERT INTO Books (pub_id, edition, ISBN, topic) VALUES
+	(1001, 2, '12345', 'technology');
 
--- Dumping data for table zli58.Author_write_Articles_or_Chapters: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Author_write_Articles_or_Chapters` DISABLE KEYS */;
-INSERT INTO `Author_write_Articles_or_Chapters` (`person_id`, `art_id`, `pub_id`) VALUES
-	(2, 1, 1),
-	(2, 2, 1),
-	(2, 3, 2),
-	(2, 4, 2);
-/*!40000 ALTER TABLE `Author_write_Articles_or_Chapters` ENABLE KEYS */;
+INSERT INTO Periodicals (period_id, type, periodicity, topic) VALUES
+	(1, 'magazine', 'monthly', 'health'),
+	(2, 'journal', 'monthly', 'science');
 
--- Dumping data for table zli58.Author_write_Books: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Author_write_Books` DISABLE KEYS */;
-INSERT INTO `Author_write_Books` (`person_id`, `pub_id`) VALUES
-	(10, 1),
-	(10, 2),
-	(10, 3),
-	(10, 4);
-/*!40000 ALTER TABLE `Author_write_Books` ENABLE KEYS */;
+INSERT INTO Issues (pub_id, period_id) VALUES
+	(1002, 1),
+	(1003, 2);
 
--- Dumping data for table zli58.Books: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Books` DISABLE KEYS */;
-INSERT INTO `Books` (`pub_id`, `edition`, `ISBN`, `topic`) VALUES
-	(1, 1, '123', NULL),
-	(2, 1, '234', NULL),
-	(3, 1, '345', NULL),
-	(4, 1, '456', 'topic1');
-/*!40000 ALTER TABLE `Books` ENABLE KEYS */;
+INSERT INTO Articles_Chapters (art_id, date, text, title, topic, pub_id) VALUES
+	(1, '2020-02-24', 'ABC', 'ABC', 'health', 1002),
+	(2, '2020-03-01', 'AAA', 'AAA', 'science', 1003);
 
--- Dumping data for table zli58.Distributors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Distributors` DISABLE KEYS */;
-INSERT INTO `Distributors` (`person_id`, `balance`, `contact_person`, `phone_num`, `type`, `city`, `name`, `address`) VALUES
-	(4, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
-	(5, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
-	(6, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown'),
-	(7, 100.00, 'Alice', '911', 'wholesale', 'Raleigh', 'Unknown', 'Unknown');
-/*!40000 ALTER TABLE `Distributors` ENABLE KEYS */;
+INSERT INTO Authors (person_id, type) VALUES
+	(3003, 'invited');
 
--- Dumping data for table zli58.Editors: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Editors` DISABLE KEYS */;
-INSERT INTO `Editors` (`person_id`, `type`) VALUES
-	(3, 'staff'),
-	(11, 'invited'),
-	(12, 'staff'),
-	(16, 'staff');
-/*!40000 ALTER TABLE `Editors` ENABLE KEYS */;
+INSERT INTO Author_write_Articles_or_Chapters (person_id, art_id) VALUES
+	(3003, 1),
+	(3003, 2);
 
--- Dumping data for table zli58.Editor_edit_Publications: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Editor_edit_Publications` DISABLE KEYS */;
-INSERT INTO `Editor_edit_Publications` (`person_id`, `pub_id`) VALUES
-	(12, 5),
-	(16, 2),
-	(16, 5),
-	(16, 7);
-/*!40000 ALTER TABLE `Editor_edit_Publications` ENABLE KEYS */;
+INSERT INTO Author_write_Books (person_id, pub_id) VALUES
+	(3003, 1001);
 
--- Dumping data for table zli58.Issues: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Issues` DISABLE KEYS */;
-INSERT INTO `Issues` (`pub_id`, `period_id`) VALUES
-	(5, 1),
-	(6, 1),
-	(7, 1),
-	(8, 2);
-/*!40000 ALTER TABLE `Issues` ENABLE KEYS */;
+INSERT INTO Editors (person_id, type) VALUES
+	(3001, 'staff'),
+	(3002, 'staff');
 
--- Dumping data for table zli58.Orders: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
-INSERT INTO `Orders` (`order_id`, `num_of_copy`, `date`, `price`, `shipping_cost`, `person_id`, `pub_id`) VALUES
-	(1, 1, '2020-03-17', 100.00, 100.00, 6, 1),
-	(2, 1, '2020-03-17', 100.00, 100.00, 6, 1),
-	(3, 1, '2020-03-17', 100.00, 100.00, 6, 1),
-	(4, 1, '2020-03-17', 100.00, 100.00, 6, 1);
-/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+INSERT INTO Editor_edit_Publications (person_id, pub_id) VALUES
+	(3001, 1001),
+	(3002, 1002);
 
--- Dumping data for table zli58.Payments: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Payments` DISABLE KEYS */;
-INSERT INTO `Payments` (`pay_id`, `date`, `type`, `amount`, `person_id`) VALUES
-	(1, '2020-03-17', 'one-time', 100.00, 1),
-	(2, '2020-03-17', 'monthly', 1.00, 2),
-	(3, '2020-03-17', 'weekly', 200.00, 3),
-	(4, '2020-03-15', 'one-time', 3.00, 4);
-/*!40000 ALTER TABLE `Payments` ENABLE KEYS */;
+INSERT INTO Orders (order_id, num_of_copy, order_date, delivery_date, price, shipping_cost, person_id, pub_id) VALUES
+	(4001, 30, '2020-01-02', '2020-01-15', 20.00, 30.00, 2001, 1001),
+	(4002, 10, '2020-02-05', '2020-02-15', 20.00, 15.00, 2001, 1001),
+	(4003, 10, '2020-02-10', '2020-02-25', 10.00, 15.00, 2002, 1003);
 
--- Dumping data for table zli58.Periodicals: ~4 rows (approximately)
-/*!40000 ALTER TABLE `Periodicals` DISABLE KEYS */;
-INSERT INTO `Periodicals` (`period_id`, `periodicity`, `topic`) VALUES
-	(1, 'monthly', NULL),
-	(2, 'weekly', NULL),
-	(3, 'weekly', NULL),
-	(4, 'weekly', NULL);
-/*!40000 ALTER TABLE `Periodicals` ENABLE KEYS */;
-
--- Dumping data for table zli58.Persons: ~16 rows (approximately)
-/*!40000 ALTER TABLE `Persons` DISABLE KEYS */;
-INSERT INTO `Persons` (`person_id`, `type`, `name`) VALUES
-	(1, 'admin', 'Alice'),
-	(2, 'author', 'Bob'),
-	(3, 'editor', 'Emma'),
-	(4, 'distributor', 'A'),
-	(5, 'distributor', 'B'),
-	(6, 'distributor', 'C'),
-	(7, 'distributor', 'D'),
-	(8, 'author', 'E'),
-	(9, 'author', 'F'),
-	(10, 'author', 'G'),
-	(11, 'editor', 'H'),
-	(12, 'editor', 'I'),
-	(13, 'admin', 'J'),
-	(14, 'admin', 'K'),
-	(15, 'admin', 'L'),
-	(16, 'editor', 'M');
-/*!40000 ALTER TABLE `Persons` ENABLE KEYS */;
-
--- Dumping data for table zli58.Publications: ~8 rows (approximately)
-/*!40000 ALTER TABLE `Publications` DISABLE KEYS */;
-INSERT INTO `Publications` (`pub_id`, `title`, `date`) VALUES
-	(1, 'Book1', '2020-03-17'),
-	(2, 'Book2', '2020-03-17'),
-	(3, 'Book3', '2020-03-17'),
-	(4, 'Book4', '2020-03-17'),
-	(5, 'Issue1', '2020-03-17'),
-	(6, 'Issue2', '2020-03-17'),
-	(7, 'Issue3', '2020-03-17'),
-	(8, 'Issue4', '2020-03-17');
-/*!40000 ALTER TABLE `Publications` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+INSERT INTO Payments (pay_id, date, type, amount, person_id) VALUES
+	(NULL, '2020-04-01', 'one-time', -1000.00, 3001),
+	(NULL, '2020-04-01', 'one-time', -1000.00, 3002),
+	(NULL, '2020-04-01', 'one-time', -1200.00, 3003),
+	(NULL, '2020-04-01', 'income', 630.00, 2001),
+	(NULL, '2020-04-01', 'income', 115.00, 2002);
